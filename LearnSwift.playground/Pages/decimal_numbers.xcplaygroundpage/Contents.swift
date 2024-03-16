58114d76b0b4b4347a8f5d4a2dc58f0b4b8bb017
Dwi Aji Prasetyo <73772152+aprxty3@users.noreply.github.com>
2024-03-17 01:36:05 +0700

    numbers

---
+++ LearnSwift.playground/Pages/decimal_numbers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+let dec = 1.0 + 0.2
+print(dec)
+
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+//let c = a + b
+let d = Double(a) + b
+let e = a + Int(b)
+
+let double1 = 4.1
+let double2 = 21212.2121
+let double3 = 5.0
+let int1 = 5
+
+var name = "Aji prasee"
+
+var rating = 5.0
+rating *= 2
+print(rating)
---
+++ LearnSwift.playground/Pages/numbers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+let score = 10
+let reallyBig = 100000
+
+let realBig = 100_000_000
+
+let lowerScore = score - 2
+let tambah = score + 10
+let kali = score * 2
+let kali2 = score * score
+let bagi = score / 2
+
+var counter = 10
+counter += 5
+counter -= 5
+counter *= 2
+counter /= 2
+
+let number = 120
+print(number.isMultiple(of: 2))
