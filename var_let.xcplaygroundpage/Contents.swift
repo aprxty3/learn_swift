92f62b9246be9ca4ad6f60ddee936adcadbe2f15
Dwi Aji Prasetyo <73772152+aprxty3@users.noreply.github.com>
2024-03-17 00:45:50 +0700

    Add files via upload

---
+++ var_let.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Aji"
+name = "Pras"
+name = "Tyo"
+
+let character = "oh yeeaa"
+//character = "yayayaya"
+//character = "yoyoyoy"
+
+var nameValue = "Roy"
+print(nameValue)
+
+nameValue = "Hojo"
+print(nameValue)
+
+nameValue = "Hoyo"
+print(nameValue)
