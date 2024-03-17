b008ef059b29ebbe57c6569cfba893442f9cbc0b
Dwi Aji Prasetyo <73772152+aprxty3@users.noreply.github.com>
2024-03-17 23:11:50 +0700

    String interpolarisation

---
+++ LearnSwift.playground/Pages/string_join.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+let people = "Haters"
+let action = "hate"
+let lyrics = people + " Gonna " + action + action
+print(lyrics)
+
+let luggageCode = "1" + "2" + "4" + "5"
+print(luggageCode)
+
+let name = "Aji"
+let age = 26
+let message = "Hi, my name are \(name) and I'm \(age) years old"
+print(message)
+
+let numbber = 22
+//let missionMessage = "Apollo " + numbber + " landed on the Moon"
+let missionMessage = "Apollo " + String(numbber) + " landed on the Moon"
+
+print("5 x 5 is \(5 * 5)")
