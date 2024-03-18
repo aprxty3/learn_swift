dd550fbb331cdcb8bace0298df2c45c7fe3d43bf
Dwi Aji Prasetyo <73772152+aprxty3@users.noreply.github.com>
2024-03-18 23:48:48 +0700

    Set

---
+++ LearnSwift.playground/Pages/set.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+//Using sets for fast lookup
+
+let actorsss = Set(["George Was", "Barack O", "D Trump", "Kin Rider"])
+print(actorsss)
+
+var actors = Set<String>()
+actors.insert("Aji Pras")
+actors.insert("Reza har")
+actors.insert("Ardit erwan")
+print(actors)
